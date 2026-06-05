36871593fa758f45697a0d2bcbe2103ab91a0fca
shreyasatishdeshmukh803-droid <shreyasatishdeshmukh803@gmail.com>
2026-06-05 11:50:58 +0530

    Update Solution 3.py

--- students/shreya-deshmukh/Solution 3.py
+++ students/shreya-deshmukh/Solution 3.py
@@ -1,1 +1,8 @@
+#problem 1
 
+s = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'}
+count =0
+for x in s:
+    if x not in['a','e','i','o','u']:
+        count=count+1
+print("no. of consonants=",count)
