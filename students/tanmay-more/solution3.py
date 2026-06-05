4c2742f249ea4990726995fb65d41c97b7f6f2f1
tanmaymore1705-dotcom <tanmaymore1705@gmail.com>
2026-06-05 13:14:36 +0530

    Implement vowel and consonant counting
    Added functionality to count vowels and consonants.

--- students/tanmay-more/solution3.py
+++ students/tanmay-more/solution3.py
@@ -33,3 +33,20 @@ for x in s:
     else :
         count=count+1
 print (count)
+
+#pratical no 10
+s = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j",  "k", "l", "m", "n", "o", "p", "q", "r", "s",  "t", "u", "v", "w", "x", "y", "z"}
+
+v = 0
+c= 0
+
+vowels = "aeiou"
+
+for x in s:
+    if x in vowels:
+        v = v + 1  
+    else:
+        c = c+ 1  
+
+print("Total Vowels:", v)
+print("Total Consonants:", c)
