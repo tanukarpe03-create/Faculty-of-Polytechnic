b6a52b28855720d0d9d7eb512dc8481dfe1ff954
tanukarpe03-create <tanukarpe03@gmail.com>
2026-06-05 11:41:18 +0530

    Update Tanuja-f2.py

--- students/tanuja-karpe/Tanuja-f2.py
+++ students/tanuja-karpe/Tanuja-f2.py
@@ -1,1 +1,23 @@
+#problem1 
+s={'a','b','c','d'}
+s.update({'e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','y','z'})
+print(s)
+count=0
+count=26
+v={'a','e','i','o','u'}
+for x in v:
+         if x in s:
+           count=count-1 
+print(count)       
+
+#problem2
+s={'a','b','c','d'}
+s.update({'e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','y','z'})
+print(s)
+count=0
+v={'a','e','i','o','u'}
+for x in v:
+         if x in s:
+           count=count+1 
+print(count)       
 
