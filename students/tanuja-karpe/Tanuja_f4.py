0754748d0806944d652cd1d1c954d25be0b5a6e7
tanukarpe03-create <tanukarpe03@gmail.com>
2026-06-12 11:52:14 +0530

    Update Tanuja_f4.py

--- students/tanuja-karpe/Tanuja_f4.py
+++ students/tanuja-karpe/Tanuja_f4.py
@@ -16,3 +16,24 @@ print("Rectangle:", calculate(5, 3,0))
 info="name surname age"
 a=info.split(" ") 
 print(a)
+
+#problem3
+def about_me():
+    name = input("Enter your name: ")
+    age = int(input("Enter your age: "))
+    city = input("Enter your city: ")
+    fav_lang = input("Enter your favourite programming language: ")
+    years = int(input("How many years have you been coding? "))
+
+    bio = f"""--------------------------
+**About Me**
+Name: {name}
+Age: {age}
+City: {city}
+Favourite Language: {fav_lang}
+Years Coding: {years}
+--------------------------"""
+    
+    print(bio)
+
+about_me()
